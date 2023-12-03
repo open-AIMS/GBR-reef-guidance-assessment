2149c305794c5d0dad8d5bc185cc298b78c01dac
ConnectedSystems <takuyai@gmail.com>
2023-12-04 09:02:29 +1000

    Working initial analysis

--- src/1_apply_analysis.jl
+++ src/1_apply_analysis.jl
@@ -14,6 +14,11 @@ using Glob
 using ProgressMeter
 
 
+# TODO: Move to config
+const DATA_DIR = "D:/development/ADRIA_data/spatial_datasets/Bathy data 10m"
+const RESULT_DIR = "../figs/"
+
+
 """
     suitability_func(threshold::Float64)::Function
 
@@ -34,20 +39,22 @@ function suitability_func(threshold::Float64)::Function
 end
 
 # Criteria/suitability functions
+# For some reason, wrapping conditions in functions causes `read()` to fail
+# Kept here in case it becomes useful later.
 function depth_criteria(depth)
     return (-3 .< depth .< 0.0)
 end
 function slope_criteria(slope)
     return (0.0 .< slope .< 40.0)
 end
-function supports_coral(p)
+function supports_coral(benthic)
     # Manually extracted from Raster Attribute Table
     # 0x00 = 0 = missing value
     # 0x0b = 11 = Sand
     # 0x0c = 12 = Rubble
     # 0x0d = 13 = Rock
     # 0x0f = 15 = Coral/Algae
-    return (p .∈ [[0x0d, 0x0f]])
+    return (benthic .∈ [[0x0d, 0x0f]])
 end
 
 # Image filtering functions
@@ -67,23 +74,7 @@ function c_mode(x)
     return mode(x)
 end
 
-
-# Script start
-DATA_DIR = "C:/Users/tiwanaga/development/ADRIA_data/spatial_datasets/Bathy data 10m"
-RESULT_DIR = "../figs/"
-
-gbr_benthic_path = "$(DATA_DIR)/Benthic/GBR10 GBRMP Benthic.tif"
-gbr_benthic = Raster(gbr_benthic_path, crs=EPSG(4326), mappedcrs=EPSG(32755), lazy=true)
-
-regions = [
-    "Townsville-Whitsunday",
-    "Cairns-Cooktown",
-    "Mackay-Capricorn",
-    "FarNorthern",
-]
-
-
-@showprogress desc="Analyzing..." for reg in regions
+function assess_region(reg, benthic_data)
     # Load bathymetry raster
     src_bathy_path = first(glob("*.tif", joinpath(DATA_DIR, "bathy", reg)))
     src_bathy = Raster(src_bathy_path, mappedcrs=EPSG(4326), lazy=true)
@@ -96,33 +87,67 @@ regions = [
     # to the target region (which is in EPSG:4326).
     # This is to cut down the amount of data to process
     dummy = resample(src_slope; crs=EPSG(4326))
-    src_benthic = crop(gbr_benthic; to=dummy)
+    src_benthic = crop(benthic_data; to=dummy)
     dummy = nothing  # mark for garbage collector
+
+    # Resample/reproject to match bathy/slope data (in EPSG:32755)
     src_benthic = resample(src_benthic, to=src_slope)
 
     # Source image is of 10m^2 pixels
-    # A hectare is 100x100 meters, so we're looking for contiguous areas where at least 95%
-    # some proportion (here 75 or 95%) meet criteria of
+    # A hectare is 100x100 meters, so we're looking for contiguous areas where
+    # some proportional area (here 75% or 95%) meet criteria of
     # (depth < 3, slope < 40, and habitat is Rock or Coral/Algae).
+    # 75% is assessed for comparison purposes.
+    # suitable = read(
+    #     depth_criteria(src_bathy) .& slope_criteria(src_slope) .& supports_coral(src_benthic)
+    # )
+
+    # See comment above re suitability functions - use of functions breaks `read()`
     suitable = read(
-        depth_criteria(src_bathy) .&
-        slope_criteria(src_slope) .&
-        supports_coral(src_benthic)
+        ((-3 .< src_bathy .< 0.0) .& 
+         (0.0 .< src_slope .< 40.0) .& 
+         (src_benthic .∈ [[0x0d, 0x0f]]))
     )
 
-    res75 = mapwindow(meets_suitability_threshold(0.75), suitable, (-4:5, -4:5), border=Fill(0)) .|> Gray
-    result_raster = res75
-    write(joinpath(RESULT_DIR, "$(reg)_suitability75.tif"), result_raster, force=true)
-    res75 = nothing  # mark for garbage collector
+    res75 = mapwindow(suitability_func(0.75), suitable, (-4:5, -4:5), border=Fill(0)) .|> Gray
+    
+    # Need a copy of raster data type to support writing to `tif`
+    result_raster = convert.(Int32, copy(suitable))
+    result_raster .= res75
 
-    res95 = mapwindow(meets_suitability_threshold(0.95), suitable, (-4:5, -4:5), border=Fill(0)) .|> Gray
-    write(joinpath(RESULT_DIR, "$(reg)_suitability95.tif"), res95, force=true)
+    write(joinpath(RESULT_DIR, "$(reg)_suitability75.tif"), result_raster; force=true)
+
+    res95 = mapwindow(suitability_func(0.95), suitable, (-4:5, -4:5), border=Fill(0)) .|> Gray
+    result_raster .= res95
+    write(joinpath(RESULT_DIR, "$(reg)_suitability95.tif"), result_raster; force=true)
 
     result_raster .= label_components(res95)
-    write(joinpath(RESULT_DIR, "$(reg)_grouped_suitability.tif"), result_raster, force=true)
+    write(joinpath(RESULT_DIR, "$(reg)_grouped_suitability.tif"), result_raster; force=true)
 
     # Apply mode filter on ~double the area
+    # Removes many groups that have a small number of relevant centroids
     mode_res = mapwindow(c_mode, result_raster, (-9:10, -9:10))
     result_raster .= label_components(mode_res)
-    write(joinpath(RESULT_DIR, "$(reg)_grouped_filtered_suitability.tif"), result_raster, force=true)
+    write(joinpath(RESULT_DIR, "$(reg)_grouped_filtered_suitability.tif"), result_raster; force=true)
+
+    # res75 = nothing
+    # res95 = nothing
+    # result_raster = nothing
+end
+
+
+# Script start 
+gbr_benthic_path = "$(DATA_DIR)/Benthic/GBR10 GBRMP Benthic.tif"
+gbr_benthic = Raster(gbr_benthic_path, crs=EPSG(4326), mappedcrs=EPSG(32755), lazy=true)
+
+# Folder names (TODO: Generalize)
+regions = String[
+    "Townsville-Whitsunday",
+    "Cairns-Cooktown",
+    "Mackay-Capricorn",
+    "FarNorthern",
+]
+
+@showprogress dt=10 desc="Analyzing..." for reg in regions
+    assess_region(reg, gbr_benthic)
 end
