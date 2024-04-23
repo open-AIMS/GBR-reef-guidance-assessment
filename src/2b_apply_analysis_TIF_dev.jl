22fef5aa3fcc2ac248deb4b000c19c236e9ec182
Ryu <r.lippmann@aims.gov.au>
2024-04-23 17:12:49 +1000

    Improved memory use in assess_allen
    Clear memory as used

--- src/2b_apply_analysis_TIF_dev.jl
+++ src/2b_apply_analysis_TIF_dev.jl
@@ -22,8 +22,7 @@ function mask_by_class(raster::AbstractRaster; geo_df::Any, classes::Vector{Stri
     filtered_geo_df = filter(row -> in(row.class, classes), geo_df)
     
     # Mask raster with filtered GeoDataFrame
-    masked_raster = Rasters.mask(raster; with=filtered_geo_df)
-    
+    masked_raster = Rasters.mask(raster; with=filtered_geo_df)    
     return masked_raster
 end    
 
@@ -34,30 +33,29 @@ function assess_allen()         # function adapted from assess_region in 2_apply
     allen_turbid_path = first(glob("*.tif", joinpath(ALLEN_DATA_DIR, "Turbidity-Q3-2023")))
     allen_turbi = Raster(allen_turbid_path, crs=EPSG(4326), lazy=true)
 
-    allen_benthic_path = first(glob("*.geojson", joinpath(ALLEN_DATA_DIR, "Benthic-Map")))
-    allen_benthic = GDF.read(allen_benthic_path)
-
-    allen_geo_path = first(glob("*.geojson", joinpath(ALLEN_DATA_DIR, "Geomorphic-Map")))
-    allen_geo = GDF.read(allen_geo_path)
-
     ## update a/b values ##
     suitable_areas = read(
         (-9.0 .<= allen_bathy .<= -2.0) .&  # depth criteria
         (allen_turbi .<= 52)    # LOW Turbidity = “52” in the turbidity maps = “5.2 FNU”
         # .& wave data
-    )
+    )    
     allen_bathy = nothing
-        GC.gc()
+    GC.gc()
     allen_turbi = nothing
-        GC.gc()
+    GC.gc()
 
+    allen_benthic_path = first(glob("*.geojson", joinpath(ALLEN_DATA_DIR, "Benthic-Map")))
+    allen_benthic = GDF.read(allen_benthic_path)
     suitable_benthic = mask_by_class(suitable_areas, allen_benthic, BENTHIC_ALLEN)
-
     allen_benthic = nothing
-        GC.gc()
+    GC.gc()
 
+    allen_geo_path = first(glob("*.geojson", joinpath(ALLEN_DATA_DIR, "Geomorphic-Map")))
+    allen_geo = GDF.read(allen_geo_path)
     suitable_flats = mask_by_class(suitable_benthic, allen_geo, FLATS_ALLEN)        
     suitable_slopes = mask_by_class(suitable_benthic, allen_geo, SLOPES_ALLEN)
+    allen_geo = nothing
+    GC.gc()
 
     ###############################################
     #### RUNS TO HERE ####
