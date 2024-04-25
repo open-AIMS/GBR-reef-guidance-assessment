eae885b938f186931cfc03765421a58dca4879cb
Takuya Iwanaga <takuyai@gmail.com>
2024-04-25 13:49:14 +1000

    Clean up common

--- src/common.jl
+++ src/common.jl
@@ -1,45 +1,30 @@
 using Distributed
+using TOML
 
-const DATA_DIR = "D:/development/ADRIA_data/spatial_datasets/Bathy data 10m"
-const RESULT_DIR = "../figs/"
-const QGIS_DIR = "../qgis/"
+try
+    global CONFIG = TOML.parsefile(".config.toml")
+catch err
+    if occursin("No such file", string(err))
+        @info "Configuration file `.config.toml` not found. See README for instructions."
+    end
 
-# Folder names (TODO: Generalize)
-REGIONS = String[
-    "Townsville-Whitsunday",
-    "Cairns-Cooktown",
-    "Mackay-Capricorn",
-    "FarNorthern",
-]
+    rethrow(err)
+end
 
-# Manually extracted from Raster Attribute Table(s)
-FLAT_IDS = [13, 14, 23]  # Inner Reef Flat, Outer Reef Flat, Plateau
-SLOPE_IDS = [21, 22, 24]  # Sheltered Reef Slope, Reef Slope, Back Reef Slope
-BENTHIC_IDS = [0x0d, 0x0f]  # Rock, Coral/Algae
+if nworkers() == 1 && (CONFIG["processing"]["N_PROCS"] > 1)
+    addprocs(CONFIG["processing"]["N_PROCS"]; dir=@__DIR__)
+end
 
-if nworkers() == 1
-    addprocs(2)
-    # addprocs(4, exeflags = "--project=$(Base.active_project())")
-
-    @everywhere begin
-        const DATA_DIR = "D:/development/ADRIA_data/spatial_datasets/Bathy data 10m"
-        const RESULT_DIR = "../figs/"
-        const QGIS_DIR = "../qgis/"
-
-        # Folder names (TODO: Generalize)
-        REGIONS = String[
-            "Townsville-Whitsunday",
-            "Cairns-Cooktown",
-            "Mackay-Capricorn",
-            "FarNorthern",
-        ]
-
-        # Manually extracted from Raster Attribute Table(s)
-        FLAT_IDS = [13, 14, 23]  # Inner Reef Flat, Outer Reef Flat, Plateau
-        SLOPE_IDS = [21, 22, 24]  # Sheltered Reef Slope, Reef Slope, Back Reef Slope
-        BENTHIC_IDS = [0x0d, 0x0f]  # Rock, Coral/Algae
+@everywhere begin
+    @eval begin
+        using TOML
+        using Statistics, StatsBase
+        using Glob
+        using ProgressMeter
+        using FLoops
 
         using Rasters
+        using NCDatasets
 
         import GeoDataFrames as GDF
         import GeoFormatTypes as GFT
@@ -49,9 +34,34 @@ if nworkers() == 1
         using ImageFiltering
         using ImageContrastAdjustment: adjust_histogram, LinearStretching
         using ImageMorphology: label_components, component_centroids
-
-        using Statistics, StatsBase
-        using Glob
-        using ProgressMeter
     end
+
+    global FIG_DIR = "../figs/"
+    global OUTPUT_DIR = "../outputs/"
+    global QGIS_DIR = "../qgis/"
+
+    global CONFIG = TOML.parsefile(".config.toml")
+    global MPA_DATA_DIR = CONFIG["mpa_data"]["MPA_DATA_DIR"]
+    global ALLEN_ATLAS_DIR = CONFIG["aca_data"]["ALLEN_ATLAS_DIR"]
+
+    # Folder names (TODO: Generalize)
+    global REGIONS = String[
+        "Townsville-Whitsunday",
+        "Cairns-Cooktown",
+        "Mackay-Capricorn",
+        "FarNorthern",
+    ]
+
+    # Manually extracted from Raster Attribute Table(s)
+    global FLAT_IDS = [13, 14, 23]  # Inner Reef Flat, Outer Reef Flat, Plateau
+    global SLOPE_IDS = [21, 22, 24]  # Sheltered Reef Slope, Reef Slope, Back Reef Slope
+    global BENTHIC_IDS = [0x0d, 0x0f]  # Rock, Coral/Algae
+
+    # Known Proj4 strings for each GBRMPA zone
+    global WAVE_REGION_CRS = Dict(
+        "Townsville-Whitsunday" => "+proj=utm +zone=55 +south +datum=WGS84",
+        "Cairns-Cooktown" => "+proj=utm +zone=55 +south +datum=WGS84",
+        "Mackay-Capricorn" => "+proj=utm +zone=56 +south +datum=WGS84",
+        "FarNorthern"=>"+proj=utm +zone=54 +south +datum=WGS84"
+    )
 end
