070a1a09c8ef947bd3e7e7a28c4bd42cf192d799
Benjamin Grier <b.grier@aims.gov.au>
2024-05-16 09:44:04 +1000

    Add development script to add buffer and visualise boat facilities
    This outputs a new geopackage of boat facilities in the GBR and their buffer polygons. The crs will not match the crs for each of the MPA raster regions.

--- src/add_pontoon_dev.jl
+++ src/add_pontoon_dev.jl
@@ -1,3 +1,5 @@
+using ColorSchemes
+
 include("common.jl")
 
 boat_data = GDF.read(joinpath(MPA_DATA_DIR,"../operational_boating_facilities.gpkg"))
@@ -51,52 +53,9 @@ Convenience plot function.
 - `gdf` : GeoDataFrame
 - `geom_col` : Column name holding geometries to plot
 - `color_by` : Column name holding factor to color reefs by (e.g. :management_area)
+- `buffer_col` : Column name holding geometries of buffer areas to plot
 """
-function plot_map(gdf::Union{DataFrame,DataFrameRow}; geom_col::Symbol=:geometry)
-    f = Figure(; size=(600, 900))
-    ga = GeoAxis(
-        f[1, 1];
-        dest="+proj=latlong +datum=WGS84",
-        xlabel="Longitude",
-        ylabel="Latitude",
-        xticklabelpad=15,
-        yticklabelpad=40,
-        xticklabelsize=10,
-        yticklabelsize=10,
-        aspect=AxisAspect(0.75),
-        xgridwidth=0.5,
-        ygridwidth=0.5,
-    )
-
-    plottable = _convert_plottable(gdf, geom_col)
-    poly!(ga, plottable)
-
-    display(f)
-
-    return f, ga
-end
-
-function plot_map!(ga::GeoAxis, gdf::DataFrame; geom_col=:geometry, color=nothing)::Nothing
-
-    plottable = _convert_plottable(gdf, geom_col)
-    if !isnothing(color)
-        poly!(ga, plottable, color=color)
-    else
-        poly!(ga, plottable)
-    end
-
-    # Set figure limits explicitly
-    xlims!(ga)
-    ylims!(ga)
-
-    return nothing
-end
-
-function plot_map!(gdf::DataFrame; geom_col=:geometry, color=nothing)::Nothing
-    return plot_map!(current_axis(), gdf; geom_col=geom_col, color=color)
-end
-
-function plot_map(gdf::Union{DataFrame,DataFrameRow}; geom_col::Symbol=:geometry, color_by::Symbol)
+function plot_map(gdf::DataFrame; geom_col::Symbol=:geometry, color_by::Symbol, buffer_col::Symbol)
     f = Figure(; size=(600, 900))
     ga = f[1, 1] = GeoAxis(
         f[1, 1];
@@ -113,7 +72,7 @@ function plot_map(gdf::Union{DataFrame,DataFrameRow}; geom_col::Symbol=:geometry
     )
 
     plottable = _convert_plottable(gdf, geom_col)
-    plottable_buffer = _convert_plottable(gdf, :buffer)
+    plottable_buffer = _convert_plottable(gdf, buffer_col)
 
     gdf[:, color_by] .= ifelse.(ismissing.(gdf[:, color_by]), "NA", gdf[:, color_by])
     gdf[:, color_by] = convert.(String, gdf[:, color_by])
@@ -131,7 +90,7 @@ function plot_map(gdf::Union{DataFrame,DataFrameRow}; geom_col::Symbol=:geometry
     names = unique(DataFrame(indices=color_indices, names=gdf[:, color_by]))
 
     # Create the unique legend entries for each level of color_by
-    unique_names = unique(names.names)
+    unique_names = names.names
     legend_entries = []
     for name in eachrow(names)
         col = palette[name.indices]
@@ -139,16 +98,17 @@ function plot_map(gdf::Union{DataFrame,DataFrameRow}; geom_col::Symbol=:geometry
         push!(legend_entries, [LE])
     end
 
-    legend_entries = Dict(zip(unique_names, legend_entries))
-
     poly!(ga, plottable_buffer, color=palette[color_indices])
     scatter!(ga, plottable, transparency=true)
 
-
-    Legend(f[2, 1], collect(values(legend_entries)), collect(keys(legend_entries)), nbanks=3,
-        tellheight=true, tellwidth=false, orientation=:horizontal, labelsize=10)
+    Legend(f[2, 1], legend_entries, unique_names, nbanks=3, tellheight=true,
+    tellwidth=false, orientation=:horizontal, labelsize=10)
 
     display(f)
 end
 
-plot_map(boat_data; geom_col=:geom, color_by=:facility)
+plot_map(relevant_points; geom_col=:geometry, color_by=:facility, buffer_col=:buffer)
+
+# Save the GBR-relevant points and the buffer zones for points. This crs is not the same as
+# MPA crs in all regions. May need reprojection before visualisation/use with each region
+GDF.write(joinpath(MPA_DATA_DIR, "../updated_boating_facilities.gpkg"), relevant_points, crs=GFT.EPSG(4326))
