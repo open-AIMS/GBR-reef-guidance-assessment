567cc0221ebf06e294b7c6d2e9f4b6ff79cf41ee
Benjamin Grier <b.grier@aims.gov.au>
2024-05-20 13:57:51 +1000

    Making 2b and 3b compatible with GDA2020
    convert these scripts to GDA2020 and add ranking of top 10 reefs to 3b_

--- src/2b_analyze_ACA.jl
+++ src/2b_analyze_ACA.jl
@@ -13,12 +13,13 @@ include("common.jl")
     Calculate the the proportion of the subsection that is suitable for deployments.
     Subsection is the surrounding hectare centred on each cell of a raster.
     """
-    function prop_suitable(subsection::AbstractMatrix)::Float32
+    function prop_suitable(subsection::AbstractMatrix)::Int16
         total = sum(subsection)
         if total == 0.0
             return 0.0
         end
-        return Float32((total / length(subsection)))
+
+        return Int16(round((total / length(subsection))*100))
     end
 
     function _write_data(fpath::String, data, cache)::Nothing
@@ -30,38 +31,45 @@ include("common.jl")
             end
             write(fpath, cache; force=true)
         end
+
         return nothing
     end
 
     function analyze_allen(reg)
-        bathy_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_bathy.tif"), lazy=true)
-        turbid_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_turbid.tif"), lazy=true)
-
-        # # Source image is of 10m^2 pixels
-        # # A hectare is 100x100 meters, so we calculate the proportional area of each hectare
-        # that meets criteria (200 <= depth <= 900 cm, and turbidty is no more than LOW (5.2 FNU))
+        bathy_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_bathy.tif"); crs=EPSG(7844), lazy=true)
+        turbid_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_turbid.tif"); crs=EPSG(7844), lazy=true)
+        waves_Hs_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_waves_Hs.tif"); crs=EPSG(7844), lazy=true)
+        waves_Tp_rst = Raster(joinpath(ACA_OUTPUT_DIR, "$(reg)_waves_Tp.tif"); crs=EPSG(7844), lazy=true)
 
         suitable_raster = read(
-            (200.0 .<= bathy_rst .<= 900.0) .&
-            (turbid_rst .<= 52)
+            (200 .<= bathy_rst .<= 900) .&
+            (0 .<= turbid_rst .<= 52) .&
+            (0.0 .<= waves_Hs_rst .<= 1.0) .&
+            (0.0 .<= waves_Tp_rst .<= 6.0)
         )
+
+        bathy_rst = nothing
+        turbid_rst = nothing
+        waves_Hs_rst = nothing
+        waves_Tp_rst = nothing
 
         # Create reef-scale raster with suitable bathy, turbidity and benthic criteria
         benthic_poly = GDF.read(joinpath(ACA_OUTPUT_DIR, "aca_benthic_$(reg).gpkg"))
         suitable_benthic = Rasters.trim(mask(suitable_raster; with=benthic_poly, boundary=:touches))
 
         # Need a copy of raster data type to support writing to `tif`
-        result_raster = convert.(Float32, copy(suitable_benthic))
+        result_raster = convert.(Int16, copy(suitable_benthic))
         rebuild(result_raster; missingval=0)
 
         # Flats
         target_flats_poly = GDF.read(joinpath(ACA_OUTPUT_DIR, "aca_target_flats_$(reg).gpkg"))
         suitable_flats = Rasters.mask(suitable_benthic; with=target_flats_poly, boundary=:touches)
 
-        res = mapwindow(prop_suitable, suitable_flats, (-4:5, -4:5), border=Fill(0)) .|> Gray #needs to be changed to calculate suitability for hectare not 37m^2
+        res = mapwindow(prop_suitable, suitable_flats, (-4:5, -4:5), border=Fill(0))
         fpath = joinpath(ACA_OUTPUT_DIR, "$(reg)_suitable_flats.tif")
         _write_data(fpath, res, result_raster)
 
+        suitable_flats = nothing
         res = nothing
         GC.gc()
 
@@ -69,7 +77,7 @@ include("common.jl")
         target_slopes_poly = GDF.read(joinpath(ACA_OUTPUT_DIR, "aca_target_slopes_$(reg).gpkg"))
         suitable_slopes = Rasters.mask(suitable_benthic; with=target_slopes_poly, boundary=:touches)
 
-        res = mapwindow(prop_suitable, suitable_slopes, (-4:5, -4:5), border=Fill(0)) .|> Gray
+        res = mapwindow(prop_suitable, suitable_slopes, (-4:5, -4:5), border=Fill(0))
         fpath = joinpath(ACA_OUTPUT_DIR, "$(reg)_suitable_slopes.tif")
         _write_data(fpath, res, result_raster)
 
